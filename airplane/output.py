97577e75b7ca4388f6b64e6fb09e884afffba648
Zhan Xiong Chin <zhan@airplane.dev>
2022-06-16 04:17:15 +0900

    Convert NaN values to null in python output (#28)
    Python generates `NaN`s when dumping JSON, which breaks our output parsing on the backend. This does some trickery to convert those into `null` values instead.
    
    It's a little hacky, the alternatives are to either 1) make the golang JSON parser work on NaN values, which doesn't seem easy or 2) import a different json encoder in python (e.g. `simplejson`), which doesn't seem ideal either since it bloats the dependencies unnecessarily

--- airplane/output.py
+++ airplane/output.py
@@ -16,7 +16,7 @@ def set_output(value: Any, *path: Union[str, int]) -> None:
         value: The value to output.
         path: Variadic parameter that denotes the subpath of the output.
     """
-    val = json.dumps(value, separators=(",", ":"))
+    val = __json_dumps(value)
     __chunk_print(f"airplane_output_set{__to_output_path(path)} {val}")
 
 
@@ -27,7 +27,7 @@ def append_output(value: Any, *path: Union[str, int]) -> None:
         value: The value to output.
         path: Variadic parameter that denotes the subpath of the output.
     """
-    val = json.dumps(value, separators=(",", ":"))
+    val = __json_dumps(value)
     __chunk_print(f"airplane_output_append{__to_output_path(path)} {val}")
 
 
@@ -42,7 +42,7 @@ def write_output(value: Any) -> None:
     Args:
         value: The value to output.
     """
-    val = json.dumps(value, separators=(",", ":"))
+    val = __json_dumps(value)
     __chunk_print(f"airplane_output {val}")
 
 
@@ -58,7 +58,7 @@ def write_named_output(name: str, value: Any) -> None:
         name: The identifier to tag the output with.
         value: The value to output.
     """
-    val = json.dumps(value, separators=(",", ":"))
+    val = __json_dumps(value)
     __chunk_print(f'airplane_output:"{name}" {val}')
 
 
@@ -76,3 +76,19 @@ def __chunk_print(output: str) -> None:
     for start in range(0, len(output), _CHUNK_SIZE):
         print(f"airplane_chunk:{chunk_key} {output[start:start+_CHUNK_SIZE]}")
     print(f"airplane_chunk_end:{chunk_key}")
+
+
+def __json_dumps(value: Any) -> str:
+    # The backend can't handle NaNs or Infs, so we have to convert these to null
+    # values. It's kind of messy to do this out-of-the-box in Python, but we can
+    # get it working the following way:
+    #
+    # We try to dump using allow_nan=False. If that fails, then we catch the
+    # resulting ValueError and then dump allowing NaNs but parse it while
+    # converting NaNs to None via parse_constant, before re-dumping it.
+    try:
+        return json.dumps(value, separators=(",", ":"), allow_nan=False)
+    except ValueError:
+        json_str = json.dumps(value, separators=(",", ":"))
+        json_with_nones = json.loads(json_str, parse_constant=lambda constant: None)
+        return json.dumps(json_with_nones, separators=(",", ":"))
--- tests/airplane/test_output.py
+++ tests/airplane/test_output.py
@@ -2,7 +2,7 @@ from typing import Any, Iterable
 
 import pytest
 
-from airplane.output import __to_output_path
+from airplane.output import __json_dumps, __to_output_path
 
 
 @pytest.mark.parametrize(
@@ -18,3 +18,14 @@ from airplane.output import __to_output_path
 )
 def test_to_output_path(path: Iterable[Any], expected_output: str) -> None:
     assert __to_output_path(path) == expected_output
+
+
+@pytest.mark.parametrize(
+    "json_value,expected_output",
+    [
+        ([], "[]"),
+        ([float("Infinity"), 3.0, float("nan")], "[null,3.0,null]"),
+    ],
+)
+def test_json_dumps(json_value: Iterable[Any], expected_output: str) -> None:
+    assert __json_dumps(json_value) == expected_output
