f47da9b4b3c4415f5af0e25eed7605764ab4b629
Ameya Khare <ameya@airplane.dev>
2022-07-26 09:51:14 -0700

    Move builtins to own module (#39)
    Refactoring change to move builtins to own module to remove root clutter.

--- airplane/__init__.py
+++ airplane/__init__.py
@@ -1,9 +1,9 @@
 """airplane - An SDK for writing Airplane tasks in Python"""
 
-from airplane import email, mongodb, rest, slack, sql
 from airplane._version import __version__
 from airplane.api.client import APIClient
 from airplane.api.types import Run, RunStatus
+from airplane.builtins import email, mongodb, rest, slack, sql
 from airplane.exceptions import InvalidEnvironmentException, RunPendingException
 from airplane.output import append_output, set_output, write_named_output, write_output
 from airplane.runtime import execute
