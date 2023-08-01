6f879385fa2d6cc6bea06cbdebbe18193cc9918b
Stephen Zhou <step.zhou@gmail.com>
2023-08-01 10:23:51 -0400

    Support message option in Slack built-in (#139)
    This supports the new Slack message option pass-through in the Python SDK.
    
    Tested this by deploying the example the new functionality
    
    ```
    import airplane
    
    
    @airplane.task()
    def python_sdk_standard() -> None:
        """This task uses the standard Python SDK to execute a SQL query and display the results."""
        airplane.slack.message("test-noise", "Hello from Python SDK!")
        airplane.slack.message(
            "test-noise", airplane.slack.MessageOption(text="Hello from Python SDK!")
        )
        airplane.slack.message(
            "test-noise",
            airplane.slack.MessageOption(
                blocks=[
                    {
                        "type": "header",
                        "text": {"type": "plain_text", "text": "New request"},
                    },
                    {
                        "type": "section",
                        "fields": [
                            {"type": "mrkdwn", "text": "*Type:*\nPaid Time Off"},
                            {
                                "type": "mrkdwn",
                                "text": "*Created by:*\n<example.com|Fred Enriquez>",
                            },
                        ],
                    },
                    {
                        "type": "section",
                        "fields": [{"type": "mrkdwn", "text": "*When:*\nAug 10 - Aug 13"}],
                    },
                    {
                        "type": "section",
                        "text": {
                            "type": "mrkdwn",
                            "text": "<https://example.com|View request>",
                        },
                    },
                ]
            ),
        )
        airplane.slack.message("U02QL6ASZPV", "Hello from Python SDK!")
        return None
    ```
    
    Resolves CAP-2924

--- airplane/builtins/slack.py
+++ airplane/builtins/slack.py
@@ -1,20 +1,38 @@
 import textwrap
-from typing import cast
+from dataclasses import asdict, dataclass
+from typing import Any, Dict, List, Optional, Union, cast
+
+from typing_extensions import Literal
 
 from airplane.api.entities import BuiltInRun
 from airplane.runtime import __execute_internal
 
 
+@dataclass
+class MessageOption:
+    """The options for the Slack message builtin."""
+
+    attachments: Optional[List[Dict]] = None
+    text: Optional[str] = None
+    blocks: Optional[List[Dict]] = None
+    mrkdwn: Optional[bool] = None
+    parse: Optional[Literal["full", "none"]] = None
+    reply_broadcast: Optional[bool] = None
+    thread_ts: Optional[str] = None
+    unfurl_links: Optional[bool] = None
+    unfurl_media: Optional[bool] = None
+
+
 def message(
     channel_name: str,
-    message: str,  # pylint: disable=redefined-outer-name
+    message: Union[str, MessageOption],  # pylint: disable=redefined-outer-name
     dedent: bool = True,
 ) -> BuiltInRun[None]:
     """Runs the builtin message function against a Slack Airplane resource.
 
     Args:
         channel_name: The slack channel to send a message to.
-        message: The message to send to the slack channel.
+        message: The message or message option to send to the slack channel.
         dedent: Whether or not to omit leading whitespace from `message`.
 
     Returns:
@@ -24,16 +42,25 @@ def message(
         HTTPError: If the message builtin cannot be executed properly.
         RunTerminationException: If the run fails or is cancelled.
     """
-    if dedent:
-        message = textwrap.dedent(message)
+    param_values: Dict[str, Any]
+    if isinstance(message, str):
+        if dedent:
+            message = textwrap.dedent(message)
+        param_values = {
+            "channelName": channel_name,
+            "message": message,
+        }
+    else:
+        param_values = {
+            "channelName": channel_name,
+            "message": "",
+            "messageOption": asdict(message),
+        }
     return cast(
         BuiltInRun[None],
         __execute_internal(
             "airplane:slack_message",
-            {
-                "channelName": channel_name,
-                "message": message,
-            },
+            param_values,
             {"slack": "res00000000zteamslack"},
         ),
     )
